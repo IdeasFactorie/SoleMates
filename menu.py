53fd2b1199fc0f4c85ddbb34102f1f639f9f3e13
Sam Kidman <samuel@netwealth.com.au>
2018-05-12 17:30:20 +1000

    reorganise code

--- menu.py
+++ menu.py
@@ -1,34 +1,83 @@
-# Reference tutorial: https://nebelprog.wordpress.com/2013/08/14/create-a-simple-game-menu-with-pygame-pt-1-writing-the-menu-options-to-the-screen/
-
 import pygame
+from settings import *
 
-pygame.init()
+class MenuItem:
+    def __init__(self, menu, label, activate_func, disabled=False):
+        self.disabled = disabled
+        self.label = label
+        self._menu = menu
+        self._activate_func = activate_func
+
+    def activate(self):
+        self._activate_func(self._menu)
 
 
-class Menu():
+class Menu:
     def __init__(self,
-                 game,
-                 screen_dim,
+                 screen,
+                 clock,
                  bg_color=(252, 179, 53),
                  font='helsinki.ttf',
                  font_size=60,
-                 font_color=(0, 0, 0)):
-        (_, _, self._screen_width, self._screen_height) = screen_dim
+                 font_color=(0, 0, 0),
+                 disabled_color=(100, 100, 100)):
 
+        self._screen_width, self._screen_height = (WIDTH, HEIGHT)
+        self.screen = screen
+        self.clock = clock
+        self._disabled_color = disabled_color
         self._font_color = font_color
         self.bg_color = bg_color
         self.font_size = font_size
-        self.font = pygame.font.SysFont(font, font_size)
-        self.font_color = font_color
+        self.font = pygame.font.Font(font, font_size)
         self._items = []
         self._label_strings = ["New Game", "Level Select", "Options", "Exit"]
-        game.register_render_func(self._render)
+        self._cur_menu = self._top_menu
 
-    def _get_items(self):
+        self._menus = {
+            "TOP_MENU": self._get_items(self._top_menu),
+            "DIFFICULTY_MENU": self._get_items(self._difficulty_select_menu)
+
+        }
+
+        self._top_menu_items = self._get_items(self._top_menu)
+        self._difficulty_menu_items = self._get_items(self._difficulty_select_menu)
+
+    def _new_game(self):
+        pass
+
+    def _level_select(self):
+        pass
+
+    def _options(self):
+        pass
+
+    def _exit(self):
+        pass
+
+    def _top_menu(self):
+        return [
+            MenuItem(self, "New Game", self._new_game),
+            MenuItem(self, "Level Select", self._level_select),
+            MenuItem(self, "Options", self._options),
+            MenuItem(self, "Exit", self._exit)
+        ]
+
+    def _difficulty_select_menu(self):
+        return [
+            MenuItem(self, "Easy", self._set_easy_difficulty),
+            MenuItem(self, "Medium", self._set_easy_difficulty),
+            MenuItem(self, "Hard", self._set_easy_difficulty)
+        ]
+
+    def _get_items(self, menu_func):
         items = []
 
-        for index, label_string in enumerate(self._label_strings):
-            label_surface = self.font.render(label_string, 1, self._font_color)
+        for index, menu_item in enumerate(menu_func()):
+            if menu_item.disabled:
+                label_surface = self.font.render(menu_item.label, 1, self._disabled_color)
+            else:
+                label_surface = self.font.render(menu_item.label, 1, self._font_color)
 
             (_, _, label_w, label_h) = label_surface.get_rect()
 
@@ -39,8 +88,25 @@ class Menu():
 
         return items
 
-    def _render(self, screen):
-        screen.fill(self.bg_color)
+    def _handle_mouse_move(self):
+        pass
 
-        for label_surface, (posx, posy) in self._get_items():
-            screen.blit(label_surface, (posx, posy))
+    def _handle_mouse_click(self):
+        pass
+
+    def _render_header(self, screen):
+        pass
+
+    def _set_easy_difficulty(self):
+        pass
+
+    def _render(self):
+        self.screen.fill(self.bg_color)
+
+        for label_surface, (posx, posy) in self._get_items(self._cur_menu):
+            self.screen.blit(label_surface, (posx, posy))
+
+    def run(self):
+        self.clock.tick(FPS)
+        self._render()
+        pygame.display.flip()
